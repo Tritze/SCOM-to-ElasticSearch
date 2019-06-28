a289744b7cd87015685eab09ee49c45f095d3d65
Timo Hagenlocher <hagenlocher.timo@gmail.com>
2019-06-28 11:19:53 +0000

    Inital from dev system

---
+++ AlertToEs.ps1
@@ -0,0 +1,129 @@
+﻿function Correct-DateTimeValue {
+     param(
+        [parameter(Mandatory=$true)]$input
+     )
+     if ($input -eq $null) {
+        return ""
+     } 
+     else {
+        return $input.ToString("u");
+     }
+}
+
+function Create-ESBody {
+     param(
+        [parameter(Mandatory=$true)]$alert
+     )
+
+    $bodyObject = @{
+        Name                              = $alert.Name;
+        Description                       = $alert.Description;
+        UnformattedDescription            = $alert.UnformattedDescription;
+        MonitoringObjectId                = $alert.MonitoringObjectId;
+        MonitoringClassId                 = $alert.MonitoringClassId;
+        ClassId                           = $alert.ClassId;
+        MonitoringObjectDisplayName       = $alert.MonitoringObjectDisplayName;
+        MonitoringObjectName              = $alert.MonitoringObjectName;
+        MonitoringObjectPath              = $alert.MonitoringObjectPath;
+        MonitoringObjectFullName          = $alert.MonitoringObjectFullName;
+        IsMonitorAlert                    = $alert.IsMonitorAlert;
+        ProblemId                         = $alert.ProblemId;
+        MonitoringRuleId                  = $alert.MonitoringRuleId;
+        RuleId                            = $alert.RuleId;
+        ResolutionState                   = $alert.ResolutionState;
+        Priority                          = $alert.Priority;
+        Severity                          = $alert.Severity;
+        Category                          = $alert.Category;
+        Owner                             = $alert.Owner;
+        ResolvedBy                        = $alert.ResolvedBy;
+        TimeRaised                        = $alert.TimeRaised;
+        TimeAdded                         = $alert.TimeAdded;
+        LastModified                      = $alert.LastModified;
+        LastModifiedBy                    = $alert.LastModifiedBy;
+        TimeResolved                      = $alert.TimeResolved;
+        TimeResolutionStateLastModified   = $alert.TimeResolutionStateLastModified;
+        CustomField1                      = $alert.CustomField1;
+        CustomField2                      = $alert.CustomField2;
+        CustomField3                      = $alert.CustomField3;
+        CustomField4                      = $alert.CustomField4;
+        CustomField5                      = $alert.CustomField5;
+        CustomField6                      = $alert.CustomField6;
+        CustomField7                      = $alert.CustomField7;
+        CustomField8                      = $alert.CustomField8;
+        CustomField9                      = $alert.CustomField9;
+        CustomField10                     = $alert.CustomField10;
+        TicketId                          = $alert.TicketId;
+        Context                           = $alert.Context;
+        ConnectorId                       = $alert.ConnectorId;
+        LastModifiedByNonConnector        = $alert.LastModifiedByNonConnector;
+        MonitoringObjectInMaintenanceMode = $alert.MonitoringObjectInMaintenanceMode;
+        MonitoringObjectHealthState       = $alert.MonitoringObjectHealthState;
+        ConnectorStatus                   = $alert.ConnectorStatus;
+        RepeatCount                       = $alert.RepeatCount;
+        NetbiosComputerName               = $alert.NetbiosComputerName;
+        NetbiosDomainName                 = $alert.NetbiosDomainName;
+        PrincipalName                     = $alert.PrincipalName;
+        SiteName                          = $alert.SiteName;
+        MaintenanceModeLastModified       = $alert.MaintenanceModeLastModified;
+        StateLastModified                 = $alert.StateLastModified;
+        Parameters                        = $alert.Parameters;
+        TfsWorkItemId                     = $alert.TfsWorkItemId;
+        TfsWorkItemOwner                  = $alert.TfsWorkItemOwner;
+        #ManagementGroupName               = $alert.ManagementGroup;
+        ManagementGroupId                 = $alert.ManagementGroupId;  
+    }
+
+    if ($alert.TimeRaised -ne $null) {
+        $bodyObject.TimeRaised  = $alert.TimeRaised.ToString("s"); 
+    }
+    if ($alert.TimeAdded -ne $null) {
+        $bodyObject.TimeAdded  = $alert.TimeAdded.ToString("s"); 
+    }
+    if ($alert.LastModified -ne $null) {
+        $bodyObject.LastModified  = $alert.LastModified.ToString("s"); 
+    }
+    if ($alert.TimeResolved -ne $null) {
+        $bodyObject.TimeResolved  = $alert.TimeResolved.ToString("s"); 
+    }
+    if ($alert.TimeResolutionStateLastModified -ne $null) {
+        $bodyObject.TimeResolutionStateLastModified  = $alert.TimeResolutionStateLastModified.ToString("s"); 
+    }
+    if ($alert.LastModifiedByNonConnector -ne $null) {
+        $bodyObject.LastModifiedByNonConnector  = $alert.LastModifiedByNonConnector.ToString("s"); 
+    }
+    if ($alert.MaintenanceModeLastModified -ne $null) {
+        $bodyObject.MaintenanceModeLastModified  = $alert.MaintenanceModeLastModified.ToString("s"); 
+    }
+    if ($alert.StateLastModified -ne $null) {
+        $bodyObject.StateLastModified  = $alert.StateLastModified.ToString("s"); 
+    }
+
+    return $bodyObject
+}
+
+function Send-ESDocument {
+     param(
+        [parameter(Mandatory=$false)]$protocol = "http",
+        [parameter(Mandatory=$false)]$server = "elastic01",
+        [parameter(Mandatory=$false)]$port = "9200",
+        [parameter(Mandatory=$false)]$index = "scomespoc",
+        [parameter(Mandatory=$false)]$type = "alert",
+        [parameter(Mandatory=$true)]$document = "",
+        [parameter(Mandatory=$true)]$body = ""
+     )
+    $method = "PUT"
+    $uri = $protocol + "://" + $server + ":" + $port + "/" + $index + "/" + $type + "/" + $document
+
+    $body = ConvertTo-Json $body
+
+    Invoke-WebRequest -Uri $uri -Body $body -Method $method -ContentType application/json
+}
+
+Import-Module OperationsManager
+
+$alerts = Get-SCOMAlert 
+
+$alerts | foreach {
+    $body = Create-ESBody -alert $_   
+    Send-ESDocument -document $_.Id -body $body
+}
---
+++ CreateIndex.ps1
@@ -0,0 +1,190 @@
+﻿PUT /scomespoc
+{
+    "settings": {
+        "index": {
+            "number_of_shards": 3,
+            "number_of_replicas": 2
+        }
+    },
+    "mappings": {
+        "alert": {
+            "properties": {
+                "Name": {
+                    "type": "text"
+                }, 
+                "Description": {
+                    "type": "text"
+                },
+                "UnformattedDescription": {
+                    "type": "text"
+                }, 
+                "MonitoringObjectId": {
+                    "type": "keyword"
+                },
+                "MonitoringClassId": {
+                    "type": "keyword"
+                },
+
+                "ClassId": {
+                    "type": "keyword"
+                }, 
+                "MonitoringObjectDisplayName": {
+                    "type": "text"
+                },
+                "MonitoringObjectName": {
+                    "type": "text"
+                }, 
+                "MonitoringObjectPath": {
+                    "type": "text"
+                },
+                "MonitoringObjectFullName": {
+                    "type": "text"
+                },
+
+                "IsMonitorAlert": {
+                    "type": "boolean"
+                }, 
+                "ProblemId": {
+                    "type": "keyword"
+                },
+                "MonitoringRuleId": {
+                    "type": "keyword"
+                }, 
+                "RuleId": {
+                    "type": "keyword"
+                },
+                "ResolutionState": {
+                    "type": "short"
+                },
+
+                "Priority": {
+                    "type": "keyword"
+                }, 
+                "Severity": {
+                    "type": "keyword"
+                },
+                "Category": {
+                    "type": "keyword"
+                }, 
+                "Owner": {
+                    "type": "text"
+                },
+                "ResolvedBy": {
+                    "type": "keyword"
+                },
+
+                "TimeRaised": {
+                    "type": "date"
+                }, 
+                "TimeAdded": {
+                    "type": "date"
+                },
+                "LastModified": {
+                    "type": "date"
+                }, 
+                "LastModifiedBy": {
+                    "type": "text"
+                },
+                "TimeResolved": {
+                    "type": "date"
+                },
+
+                "TimeResolutionStateLastModified": {
+                    "type": "date"
+                }, 
+                "CustomField1": {
+                    "type": "text"
+                },
+                "CustomField2": {
+                    "type": "text"
+                }, 
+                "CustomField3": {
+                    "type": "text"
+                },
+                "CustomField4": {
+                    "type": "text"
+                },
+
+                "CustomField5": {
+                    "type": "text"
+                }, 
+                "CustomField6": {
+                    "type": "text"
+                },
+                "CustomField7": {
+                    "type": "text"
+                }, 
+                "CustomField8": {
+                    "type": "text"
+                },
+                "CustomField9": {
+                    "type": "text"
+                },
+
+                "CustomField10": {
+                    "type": "text"
+                }, 
+                "TicketId": {
+                    "type": "keyword"
+                },
+                "Context": {
+                    "type": "text"
+                }, 
+                "ConnectorId": {
+                    "type": "keyword"
+                },
+                "LastModifiedByNonConnector": {
+                    "type": "date"
+                },
+
+                "MonitoringObjectInMaintenanceMode": {
+                    "type": "boolean"
+                }, 
+                "MonitoringObjectHealthState": {
+                    "type": "keyword"
+                },
+                "ConnectorStatus": {
+                    "type": "keyword"
+                }, 
+                "RepeatCount": {
+                    "type": "integer"
+                },
+                "NetbiosComputerName": {
+                    "type": "keyword"
+                },
+
+                "NetbiosDomainName": {
+                    "type": "keyword"
+                }, 
+                "PrincipalName": {
+                    "type": "text"
+                },
+                "SiteName": {
+                    "type": "text"
+                }, 
+                "MaintenanceModeLastModified": {
+                    "type": "date"
+                },
+                "StateLastModified": {
+                    "type": "date"
+                },
+
+                "Parameters": {
+                    "type": "text"
+                }, 
+                "TfsWorkItemId": {
+                    "type": "keyword"
+                },
+                "TfsWorkItemOwner": {
+                    "type": "text"
+                }, 
+                "ManagementGroup": {
+                    "type": "keyword"
+                },
+                "ManagementGroupId": {
+                    "type": "keyword"
+                }
+            } 
+        }
+    } 
+}
